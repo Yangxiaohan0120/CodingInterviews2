00bfb6182164e6e6d5b009f5be0a8a67f63d9af2
牛客401498474号 <17853138921@163.com>
2022-08-10 15:19:29 +0800

    init commit

---
+++ src/main/java/org/example/No1/Solution.java
@@ -0,0 +1,11 @@
+package org.example.No1;
+
+/**
+ * @author Chris Yang
+ * created 2022-08-10 15:19
+ **/
+public class Solution {
+    public static void main(String[] args) {
+        System.out.println("hello No1");
+    }
+}
